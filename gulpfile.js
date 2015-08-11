0c76e102f171e4cf560d623e5ea4336397ed47a0
shikakun <k@u-n.jp>
2015-08-11 17:30:46 +0900

    gulp を実行して sass を css へ変換してベンダープレフィックスを追加できるようにした

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+var pleeease = require('gulp-pleeease');
+var sass = require('gulp-sass');
+var themeDir = 'themes/dev.shikakun.com/'
+
+gulp.task('sass', function() {
+  gulp.src(themeDir + 'assets/stylesheets/**/*.scss')
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(pleeease({
+      autoprefixer: {
+        browsers: [
+          'last 1 version',
+          'iOS >= 7',
+          'Android >= 4',
+          'ie >= 9'
+        ]
+      },
+      minifier: true
+    }))
+    .pipe(gulp.dest(themeDir + 'source/stylesheets/'));
+});
+
+gulp.task('default', function() {
+  gulp.watch(themeDir + 'assets/stylesheets/**/*.scss', ['sass']);
+});
