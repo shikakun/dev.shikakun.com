fa3f1d325af58ee2d720541e704f71b159b37396
shikakun <k@u-n.jp>
2015-08-12 15:52:31 +0900

    フッターのスタイルを指定した

--- gulpfile.js
+++ gulpfile.js
@@ -25,11 +25,11 @@ gulp.task('sass', function() {
 });
 
 gulp.task('copy', function() {
-  gulp.src(themeDir + 'assets/**/*.{eot,otf,woff}')
+  gulp.src(themeDir + 'assets/**/*.{eot,otf,svg,woff}')
     .pipe(gulp.dest(themeDir + 'source/'));
 });
 
 gulp.task('default', function() {
   gulp.watch(themeDir + 'assets/stylesheets/**/*.scss', ['sass']);
-  gulp.watch(themeDir + 'assets/**/*.{eot,otf,woff}', ['copy']);
+  gulp.watch(themeDir + 'assets/**/*.{eot,otf,svg,woff}', ['copy']);
 });
